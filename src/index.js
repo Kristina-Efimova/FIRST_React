bdef17b1adb4ac040c44c49fb8e65571e030bb84
Кристина <1059700@bsu.edu.ru>
2021-06-05 22:41:19 +0300

    Создание React приложения Создание простой формы

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/app/';
+// import AppHeader from "./components/app-header/app-header";
+
+
+
+// const Header = () => {
+//     return <h2>Hello World!</h2>
+// }
+//
+// const Field = () => {
+//     return <input type='text' placeholder='Type here'/>
+// }
+//
+// const Btn = () => {
+//     const text = 'Log in';
+//     return <button>{text}</button>
+// }
+//
+// const App = () => {
+//     return (
+//         <div>
+//             <Header/>
+//             <Field/>
+//             <Btn/>
+//         </div>
+//     )
+// }
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
