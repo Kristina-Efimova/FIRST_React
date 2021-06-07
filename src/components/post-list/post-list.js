08cdf1aaa426e831c65b21d792bc1fda9d18b749
Кристина <1059700@bsu.edu.ru>
2021-06-07 21:28:58 +0300

    Создание стилей с помощью reactstrap

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ListGroup } from 'reactstrap';
 
 import './post-list.css'
 
@@ -18,9 +19,9 @@ const PostList = ({posts}) => {
     });
 
     return (
-        <ul className='app-list list-group'>
+        <ListGroup className='app-list'>
             {element}
-        </ul>
+        </ListGroup>
     )
 }
 
