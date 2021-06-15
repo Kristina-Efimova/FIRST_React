f771b26115c7100f24039bfa1b333b95af009e2e
Кристина <1059700@bsu.edu.ru>
2021-06-15 20:50:19 +0300

    Реализация фильтра через кнопки Поиск по введенному значению

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -35,12 +35,16 @@ export default class App extends Component {
                     id: 3
 
                 }
-            ]
+            ],
+            term: '',
+            filter: 'all'
         };
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggleLike = this.onToggleLike.bind(this);
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+        this.onFilterSelect = this.onFilterSelect.bind(this);
 
         this.maxId = 4;
     }
@@ -102,12 +106,41 @@ export default class App extends Component {
         this.onToggleImportantAndlike(id, 'like')
     }
 
+    searchPost(items, term) {
+        if (term.length === 0) {
+            return items
+        }
+
+        return items.filter((item) => {
+            return item.label.indexOf(term) > -1
+        });
+    }
+
+    filterPost(items, filter) {
+        if (filter === 'like') {
+            return items.filter(item => item.like)
+        } else {
+            return items
+        }
+
+    }
+
+    onUpdateSearch(term) {
+        this.setState({term});
+    }
+
+    onFilterSelect(filter) {
+        this.setState({filter});
+    }
+
 
     render() {
 
-        const {data} = this.state;
+        const {data, term, filter} = this.state;
         const liked = data.filter(item => item.like).length;
         const allPost = data.length;
+        const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
+
 
         return (
             <div className='app'>
@@ -115,11 +148,14 @@ export default class App extends Component {
                     liked={liked}
                     allPost={allPost}/>
                 <div className='search-panel d-flex'>
-                    <SearchPanel/>
-                    <PostStatusFilter/>
+                    <SearchPanel
+                        onUpdateSearch={this.onUpdateSearch}/>
+                    <PostStatusFilter
+                        filter={filter}
+                        onFilterSelect={this.onFilterSelect}/>
                 </div>
                 <PostList
-                    posts={this.state.data}
+                    posts={visiblePosts}
                     onDelete={this.deleteItem}
                     onToggleImportant={this.onToggleImportant}
                     onToggleLike={this.onToggleLike}/>
